791858d774683f7884c3f3bf5633c69ba1b9ab94
shyamkumar2180@gmail.com <shyamkumar2180@gmail.com>
2022-11-02 06:27:43 +0000

    recent changes

--- models/development/dev_confirmed.sql
+++ models/development/dev_confirmed.sql
@@ -1,1 +1,30 @@
-select * from {{source('DBT_SKUMAR_STAGING','stg_customers')}}
+
+ --select  count(*)  from {{ref('dev_customer')}}
+
+
+
+-----------------------------------------------------
+-- with cte as(
+--     select phone_number,
+
+--     case
+--         when substring(phone_number,2,2)<=20 then 'A'
+--         else 'B'
+--     end as category
+--     from {{source('DBT_SKUMAR_STAGING','stg_customers')}}
+-- )
+--  select * from cte   order by category asc
+-----------------------------------------------------
+with cte as(
+    {% set category = [ "A"] %}
+select
+    phone_number,
+    {% for a in category %}
+    case when category = '{{a}}'then 'A' else 'B' end as category_type
+    {% endfor %}
+from {{ref('dev_customer')}}
+)
+select * from cte
+-- --------------------------------------------------------
+
+ 
--- models/development/dev_customer.sql
+++ models/development/dev_customer.sql
@@ -1,6 +1,29 @@
 
 
+--select * from {{source('DBT_SKUMAR_STAGING','stg_customers')}}
 
- select * from {{ref('dev_confirmed')}}
- 
+ -----------using case statment--------------------------
+with cte as(
+    select phone_number,
+    case
+        when substring(phone_number,2,2)<=20 then 'A'
+        when substring(phone_number,2,2)>20 then 'B'
+        when substring(phone_number,2,2)>40 then 'C'
+        else 'other'
+    end as category
+    from {{source('DBT_SKUMAR_STAGING','stg_customers')}}
+)
+ select * from cte   order by category asc
+------------------------------------------------------------
+
+--------------using iff statment----------------------------
+
+--select phone_number,
+--iff(substring(phone_number,2,2)<=20,'A','B') as category 
+ --from {{source('DBT_SKUMAR_STAGING','stg_customers')}}
+----------------------------------------------------------------
+
+ --select phone_number,substring(phone_number,2,2)<=20  as category
+    --from {{source('DBT_SKUMAR_STAGING','stg_customers')}}
+ ------------------------------------------------------------
 
