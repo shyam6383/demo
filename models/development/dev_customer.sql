d6acf6abe3e3ff0c7e3c0ac0aa0a18c8f5647295
shyamkumar2180@gmail.com <shyamkumar2180@gmail.com>
2022-11-02 13:10:29 +0000

    final changes

--- models/development/dev_customer.sql
+++ models/development/dev_customer.sql
@@ -1,29 +1,14 @@
-
-
---select * from {{source('DBT_SKUMAR_STAGING','stg_customers')}}
-
- -----------using case statment--------------------------
 with cte as(
-    select phone_number,
-    case
-        when substring(phone_number,2,2)<=20 then 'A'
-        when substring(phone_number,2,2)>20 then 'B'
-        when substring(phone_number,2,2)>40 then 'C'
-        else 'other'
+{% set condition = ['<=10','<20','>10','>20'] %}
+{% set val = ['A','B','B','C'] %}
+select phone_number,
+
+ case
+  {% for types , aa in  zip(condition , val) %}
+    when substring(phone_number,2,2){{types}} then '{{aa}}'
+    {% endfor %}
+    else 'others'
     end as category
     from {{source('DBT_SKUMAR_STAGING','stg_customers')}}
 )
- select * from cte   order by category asc
-------------------------------------------------------------
-
---------------using iff statment----------------------------
-
---select phone_number,
---iff(substring(phone_number,2,2)<=20,'A','B') as category 
- --from {{source('DBT_SKUMAR_STAGING','stg_customers')}}
-----------------------------------------------------------------
-
- --select phone_number,substring(phone_number,2,2)<=20  as category
-    --from {{source('DBT_SKUMAR_STAGING','stg_customers')}}
- ------------------------------------------------------------
-
+select * from cte
